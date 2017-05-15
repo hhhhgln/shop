aeca0ae99c610e83cec7c0b6677eb40e4632a60f
zhangshipeng <974600840@qq.com>
2017-05-15 16:15:28 +0800

    add

--- src/main/java/com/zknu/shop/pojo/EcsBrand.java
+++ src/main/java/com/zknu/shop/pojo/EcsBrand.java
@@ -6,7 +6,6 @@ public class EcsBrand {
     private Short brandId;
     @NotEmpty(message = "品牌名称不能为空")
     private String brandName;
-    private String name;
     private String brandLogo;
 
     private String siteUrl;
